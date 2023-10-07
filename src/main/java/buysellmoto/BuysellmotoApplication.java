0d5315f885fbf6cd87c491a95463f27ee230d27c
Hoang Dung <dungnh1091999@gmail.com>
2023-10-07 14:04:52 +0700

    new

---
+++ src/main/java/buysellmoto/BuysellmotoApplication.java
@@ -0,0 +1,13 @@
+package buysellmoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuysellmotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuysellmotoApplication.class, args);
+	}
+
+}
